b7ae30bc26c5a097f2e3b66862d312c83872186c
Huy <qhn5402@gmail.com>
2021-11-23 20:41:31 +0700

    Test tiep

--- Folder ne/server.py
+++ Folder ne/server.py
@@ -1,2 +1,17 @@
-print("Ko co gi dau, dung xem")
-print(1+1)
+import socket
+import threading
+import time
+PORT= 5050
+SERVER=socket.gethostbyname(socket.gethostname())
+ADDR=(SERVER,PORT)
+server=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+server.bind(ADDR)
+def handle_client(conn, addr):
+    pass
+def start():
+    server.listen()
+    while True:
+        conn,addr=server.accept()
+        
+print("SERVER IS STARTING...")
+start()
