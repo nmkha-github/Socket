54206d7edef332cfc61f07389418f7c32001e03d
nmkha-github <85301223+nmkha-github@users.noreply.github.com>
2021-12-12 08:27:58 +0700

    Add files via upload

---
+++ SOURCE CODE GUI/Server/convert.py
@@ -0,0 +1,19 @@
+patterns = {
+    '[àáảãạăắằẵặẳâầấậẫẩ]': 'a',
+    '[đ]': 'd',
+    '[èéẻẽẹêềếểễệ]': 'e',
+    '[ìíỉĩị]': 'i',
+    '[òóỏõọôồốổỗộơờớởỡợ]': 'o',
+    '[ùúủũụưừứửữự]': 'u',
+    '[ỳýỷỹỵ]': 'y'
+}
+import re
+def convert_to_eng(text):   #chuyển tiếng việt có dấu thành không giấu viết hoa
+    output = text
+    for regex, replace in patterns.items():
+        output = re.sub(regex, replace, output)
+        #Trường hợp viết hoa
+        output = re.sub(regex.upper(), replace.upper(), output)
+    return output.upper()
+
+# print(convert_to_eng('Thành phố Hồ chí Minh'))
